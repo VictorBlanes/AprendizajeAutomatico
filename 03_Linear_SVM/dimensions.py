db3d2094b97005e2c785f5ecefee19e637d04fe7
Victor M <vicblanescastro@gmail.com>
2023-10-05 17:07:07 +0200

    Practica 3 fase 2 done, might want to do a quick recheck just to refactor

--- 03_Linear_SVM/dimensions.py
+++ 03_Linear_SVM/dimensions.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import StandardScaler
 from Adaline import Adaline
 from sklearn.svm import SVC
 
-
 # Generació del conjunt de mostres
 X, y = make_classification(n_samples=400, n_features=5, n_redundant=0, n_repeated=0,
                            n_classes=2, n_clusters_per_class=1, class_sep=1,
@@ -15,19 +14,25 @@ X, y = make_classification(n_samples=400, n_features=5, n_redundant=0, n_repeate
 
 # Separar les dades: train_test_split
 
-# TODO
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 # Estandaritzar les dades: StandardScaler
 
-# TODO
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train_transformed = scaler.transform(X_train)
+X_test_transformed = scaler.transform(X_test)
 
 # Entrenam una SVM linear (classe SVC)
 
-# TODO
+svc = SVC(C=1.0, kernel='linear')
+svc.fit(X_train_transformed, y_train)
 
 # Prediccio
-# TODO
-
+y_test_predicted = svc.predict(X_test_transformed)
 
 # Metrica
-# TODO
+aciertos = np.sum(y_test_predicted == y_test)
+tasa_aciertos = aciertos / len(y_test_predicted)
+print(tasa_aciertos * 100.0)
